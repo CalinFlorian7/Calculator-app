6df01bfb5257537014299081da5e8020cf357fd3
RacaruCalin7 <racarucalinflorian@grmail.com>
2024-08-26 18:02:32 +0300

    route added to calculate the operation

--- src/main/java/com/example/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -1,15 +1,26 @@
 package com.example.calculator.controller;
 
+import com.example.calculator.classes.Calculator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @Controller
 public class CalculatorController {
-    @GetMapping
+    @GetMapping("/")
     String getCalculator(Model model)
     {
         model.addAttribute("title","Welcome to the calculator app");
         model.addAttribute("message","Use the calculator to perform basic operations");
+        model.addAttribute("calculator",new Calculator());
+        return "calculator";
+    }
+    @PostMapping("/calculate")
+    public String calculateOperation(@ModelAttribute Calculator calculator,Model model) {
+        calculator.calculate();
+        model.addAttribute("calculator",calculator);
         return "calculator";
     }
 
